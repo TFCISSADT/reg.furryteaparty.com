18bef1a5290495f850867c0d213c04960c10b9fb
MountainPineTW <hi@mptw.me>
2022-12-05 04:05:52 +0800

    deploy

---
+++ accountRoles.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    key: "ADMIN",
+    name: "Admin",
+    hasManagementPermission: true,
+  },
+  {
+    key: "SERVICE_DESK",
+    name: "Service Desk",
+    hasManagementPermission: true,
+  },
+  {
+    key: "VOLUNTEER",
+    name: "Volunteer",
+    hasManagementPermission: false,
+  },
+  {
+    key: "USER",
+    name: "User",
+    hasManagementPermission: false,
+  },
+];
