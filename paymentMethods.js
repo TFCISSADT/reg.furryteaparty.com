6af33ead294781acbb1728c51e6ae10f09720f05
MountainPineTW <hi@mptw.me>
2022-12-05 04:05:52 +0800

    deploy

---
+++ paymentMethods.js
@@ -0,0 +1,51 @@
+module.exports = [
+  {
+    key: "ONSITE",
+    name: "Onsite Payment",
+    icon: "desk",
+    priceRange: null,
+    excludeCountries: [],
+  },
+  {
+    key: "NEWEBPAY_ATM",
+    name: "ATM",
+    icon: "atm",
+    priceRange: [1, 49999],
+    excludeCountries: [],
+  },
+  {
+    key: "NEWEBPAY_CVS",
+    name: "超商代碼",
+    icon: "store",
+    priceRange: [1, 20000],
+    excludeCountries: [],
+  },
+  {
+    key: "NEWEBPAY_CC",
+    name: "Credit Card",
+    icon: "credit-card",
+    priceRange: null,
+    excludeCountries: [],
+  },
+  {
+    key: "OEN_ATM",
+    name: "ATM",
+    icon: "atm",
+    priceRange: [80, 30000],
+    excludeCountries: [],
+  },
+  {
+    key: "OEN_CVS",
+    name: "超商代碼",
+    icon: "store",
+    priceRange: [1, 6000],
+    excludeCountries: [],
+  },
+  {
+    key: "OEN_CC",
+    name: "Credit Card",
+    icon: "credit-card",
+    priceRange: null,
+    excludeCountries: [],
+  },
+];
